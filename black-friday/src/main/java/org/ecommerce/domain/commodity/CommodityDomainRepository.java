bf17c1bccb2f7bb98c4186e3b7fb889ff6e83cc3
Hui <105135459+Huicccc@users.noreply.github.com>
2022-12-30 01:00:02 -0500

    git error restart

--- black-friday/src/main/java/org/ecommerce/domain/commodity/CommodityDomainRepository.java
+++
@@ -1,8 +0,0 @@
-package org.ecommerce.domain.commodity;
-
-public interface CommodityDomainRepository {
-
-  CommodityDomain createCommodity(CommodityDomain commodityDomain);
-
-  CommodityDomain getCommodityById(String id);
-}
